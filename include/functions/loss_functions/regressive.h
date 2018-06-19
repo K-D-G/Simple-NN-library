74a66a3fb4d313c182ebee8793f00dcef54ca1d4
K-D-G <grayshonprogrammer@gmail.com>
2018-06-19 20:59:26 +0100

    Started regressive functions

---
+++ include/functions/loss_functions/regressive.h
@@ -0,0 +1,17 @@
+#ifndef __FUNCTIONS__LOSS_FUNCTIONS__REGRESSIVE_H
+#define __FUNCTIONS__LOSS_FUNCTIONS__REGRESSIVE_H
+#include <iostream>
+
+class Regressive{
+public:
+  float[]* list_of_functions[3]={&this.mean_square_error, &this.absolute_error, &this.smooth_absolute_error};
+
+  std::string get_func_name(float[]* func);
+  float[]* get_func(std::string name);
+
+  float[] mean_square_error(float output[], float expected_output[]);
+  float[] absolute_error(float output[], float expected_output[]);
+  float[] smooth_absolute_error(float output[], float expected_output[]);
+};
+
+#endif
---
+++ src/functions/loss_functions/regressive.cpp
@@ -0,0 +1,29 @@
+#include <functions/loss_functions/regressive.h>
+
+using namespace std;
+using namespace functions;
+using namespace loss_functions;
+using namespace the_network;
+
+string Regressive::get_func_name(float[]* func){
+  string func_name="unknown";
+  if(func==this.list_of_functions[0]){func_name="mean square error";}
+  else if(func==this.list_of_functions[1]){func_name="absolute error";}
+  else if(func==this.list_of_functions[2]){func_name="smooth absolute error";}
+  return func_name;
+}
+float[]* Regressive::get_func(string name){
+  if(name=="mean square error"){return this.list_of_functions[0];}
+  else if(name=="absolute error"){return this.list_of_functions[1];}
+  else if(name=="smooth absolute error"){return this.list_of_functions[2];}
+}
+
+float[] Regressive::mean_square_error(float output[], float expected_output[]){
+
+}
+float[] Regressive::absolute_error(float output[], float expected_output[]){
+
+}
+float[] Regressive::smooth_absolute_error(float output[], float expected_output[]){
+
+}
